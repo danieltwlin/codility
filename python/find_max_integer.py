e8b7c894a80dc76a98ce89ea0314463e40baf32c
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-04-13 13:24:49 +0800

    Create find_max_integer.py

---
+++ python/find_max_integer.py
@@ -0,0 +1,26 @@
+def find_min_positive(nums):
+	s = set(nums)
+	i=1
+	while i in s:
+		i = i + 1
+	return i
+	
+	
+if __name__=='__main__':
+
+	nums =[1, 3, 6, 4, 1, 2]
+	max = find_min_positive(nums)
+	print(max)
+	
+	nums = [1, 2, 3]
+	max = find_min_positive(nums)
+	print(max)
+	
+  nums = [1, 2, 3]
+	max = find_min_positive(nums)
+	print(max)
+  
+	nums =  [−1, −3]
+	max = find_min_positive(nums)
+	print(max)
+ 
